db3436a12cd00ec82c03015d34d058d7f03f57e1
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-16 09:43:09 +0330

    Expose Master Agent generation 100 core

--- autonomous_core/__init__.py
+++ autonomous_core/__init__.py
@@ -1,3 +1,4 @@
 from .master_core import MasterCore
+from .master_core_generation_41_100 import MasterAgent100, MasterCore100
 
-__all__ = ["MasterCore"]
+__all__ = ["MasterCore", "MasterCore100", "MasterAgent100"]
