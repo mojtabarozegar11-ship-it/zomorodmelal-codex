228cf95742b47cbe53900e11b6145a4a917decdc
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-17 12:04:18 +0330

    Add Django configuration skeleton

---
+++ django_project/config/settings.py
@@ -0,0 +1,11 @@
+"""Initial Django settings placeholder for Zomorod Melal platform."""
+
+SECRET_KEY = 'change-this-in-production'
+DEBUG = True
+
+INSTALLED_APPS = [
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+]
+
+ROOT_URLCONF = 'config.urls'
