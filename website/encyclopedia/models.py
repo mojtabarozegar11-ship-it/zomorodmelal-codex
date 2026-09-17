2312cccf29e5d1c1471c25c1ff61ff776e7d1e60
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-17 09:01:53 +0330

    Create encyclopedia data models

---
+++ website/encyclopedia/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class EncyclopediaCategory(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class EncyclopediaArticle(models.Model):
+    category = models.ForeignKey(EncyclopediaCategory, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300)
+    slug = models.SlugField(unique=True)
+    summary = models.TextField(blank=True)
+    content = models.TextField()
+    keywords = models.TextField(blank=True)
+    sources = models.TextField(blank=True)
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class KnowledgeRelation(models.Model):
+    article = models.ForeignKey(EncyclopediaArticle, on_delete=models.CASCADE)
+    related_article = models.ForeignKey(EncyclopediaArticle, related_name='relations', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.article} -> {self.related_article}'
