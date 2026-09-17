9596e5897c8b14995c55c620064c8cde26ecc3c4
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-17 12:45:16 +0330

    Connect Django API views with Agent routes

--- django_project/api/urls.py
+++ django_project/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .agent_api import agent_status, execute_goal
+from .views import agent_execute
 
 urlpatterns = [
     path("status/", agent_status),
     path("execute/", execute_goal),
+    path("prepare/", agent_execute),
 ]
