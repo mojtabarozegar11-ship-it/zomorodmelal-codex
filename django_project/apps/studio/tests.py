17d1b2003b5a9f12cda5cf96280193298c1ddebe
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-18 04:43:12 +0330

    Harden studio route test syntax

--- django_project/apps/studio/tests.py
+++ django_project/apps/studio/tests.py
@@ -1,6 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import resolve
 
+
 class StudioRouteTests(SimpleTestCase):
     def test_studio_route(self):
         self.assertEqual(resolve("/studio/").url_name, "studio-home")
