fe6c710dd41e6c0a427afac2e232ddd92d4430d4
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-18 22:09:11 +0330

    Add ERP app route

--- django_project/apps/office/urls.py
+++ django_project/apps/office/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.dashboard, name="office-dashboard")]
+urlpatterns = [path("", views.dashboard, name="office-dashboard"), path("app/", views.app_shell, name="office-app")]
