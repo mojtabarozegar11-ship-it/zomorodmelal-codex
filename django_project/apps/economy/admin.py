a9960b698cfc4329e472bc2c2e2877651afdad4a
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-18 04:57:21 +0330

    Register gold orders in admin

--- django_project/apps/economy/admin.py
+++ django_project/apps/economy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import Asset, EconomicAuditLog, EconomicPublication, EconomicReport, EconomicWorkItem, MarketDataSnapshot, OrderIntent, Portfolio, Position, RiskPolicy, Signal, SignalCoupon, SignalPerformance, SignalProduct, SignalPurchase, SignalSubscription, Strategy, Trade, VirtualAssetProject
 
-for model in (Asset, MarketDataSnapshot, Strategy, RiskPolicy, Portfolio, Position, OrderIntent, Trade, EconomicReport, EconomicAuditLog, SignalProduct, SignalPurchase, SignalSubscription, SignalCoupon, SignalPerformance, Signal, VirtualAssetProject, EconomicWorkItem, EconomicPublication):
+for model in (Asset, MarketDataSnapshot, Strategy, RiskPolicy, Portfolio, Position, OrderIntent, Trade, EconomicReport, EconomicAuditLog, SignalProduct, SignalPurchase, SignalSubscription, SignalCoupon, SignalPerformance, Signal, VirtualAssetProject, EconomicWorkItem, EconomicPublication, GoldOrder):
     admin.site.register(model)
