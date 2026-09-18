11fce681d4ecbb06769b675e4c64cbd039f3e853
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-18 04:19:54 +0330

    Register signal sales in admin

--- django_project/apps/economy/admin.py
+++ django_project/apps/economy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Asset, EconomicAuditLog, EconomicReport, MarketDataSnapshot, OrderIntent, Portfolio, Position, RiskPolicy, Strategy, Trade
+from .models import Asset, EconomicAuditLog, EconomicReport, MarketDataSnapshot, OrderIntent, Portfolio, Position, RiskPolicy, Signal, SignalProduct, SignalPurchase, Strategy, Trade
 
-for model in (Asset, MarketDataSnapshot, Strategy, RiskPolicy, Portfolio, Position, OrderIntent, Trade, EconomicReport, EconomicAuditLog):
+for model in (Asset, MarketDataSnapshot, Strategy, RiskPolicy, Portfolio, Position, OrderIntent, Trade, EconomicReport, EconomicAuditLog, SignalProduct, SignalPurchase, Signal):
     admin.site.register(model)
