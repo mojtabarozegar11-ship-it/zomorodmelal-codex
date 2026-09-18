52632debe84f0487b2e83c834b3c2aca61dc40ba
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-18 04:20:50 +0330

    Register signal memberships and performance

--- django_project/apps/economy/admin.py
+++ django_project/apps/economy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Asset, EconomicAuditLog, EconomicReport, MarketDataSnapshot, OrderIntent, Portfolio, Position, RiskPolicy, Signal, SignalProduct, SignalPurchase, Strategy, Trade
+from .models import Asset, EconomicAuditLog, EconomicReport, MarketDataSnapshot, OrderIntent, Portfolio, Position, RiskPolicy, Signal, SignalProduct, SignalPurchase, SignalSubscription, SignalCoupon, SignalPerformance, Strategy, Trade
 
-for model in (Asset, MarketDataSnapshot, Strategy, RiskPolicy, Portfolio, Position, OrderIntent, Trade, EconomicReport, EconomicAuditLog, SignalProduct, SignalPurchase, Signal):
+for model in (Asset, MarketDataSnapshot, Strategy, RiskPolicy, Portfolio, Position, OrderIntent, Trade, EconomicReport, EconomicAuditLog, SignalProduct, SignalPurchase, SignalSubscription, SignalCoupon, SignalPerformance, Signal):
     admin.site.register(model)
