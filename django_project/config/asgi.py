f30d0df423dd00783e2659b1e688607049dac223
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-19 01:23:15 +0330

    Fix host runtime syntax and imports

--- django_project/config/asgi.py
+++ django_project/config/asgi.py
@@ -1,5 +1,10 @@
+
 """ASGI config for Zomorod Melal Django project."""
+
 import os
-from django.core.asgi import get_asgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from django.core.asgi import get_asgi_application
+
 application = get_asgi_application()
