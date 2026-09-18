140243c4abf07ed04aff4c4330323dd3236ac082
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-18 22:21:22 +0330

    Harden ERP data validation and accounting invariants

--- django_project/apps/office/models.py
+++ django_project/apps/office/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db import models
+from django.db import models\nfrom django.core.exceptions import ValidationError\nfrom django.db.models import Q
 
 
 class Company(models.Model):
