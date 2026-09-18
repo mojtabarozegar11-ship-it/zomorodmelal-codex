50f872a2b1bda8f60dd168d2f4062b04ee7e1229
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-19 01:23:17 +0330

    Fix host runtime syntax and imports

--- django_project/config/wsgi.py
+++ django_project/config/wsgi.py
@@ -1,5 +1,10 @@
+
 """WSGI config for Zomorod Melal Django project."""
+
 import os
-from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
