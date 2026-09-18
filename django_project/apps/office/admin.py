fa22ed07d85e12fa43fe9f6c81d31c7e69d4a65f
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-19 01:39:37 +0330

    Register double-entry accounting in admin

--- django_project/apps/office/admin.py
+++ django_project/apps/office/admin.py
@@ -1,37 +1,14 @@
 from django.contrib import admin
 
 from .models import (
-    AccountingEntry,
-    AuditLog,
-    CashTransaction,
-    Company,
-    CompanyDelegation,
-    Employee,
-    InventoryItem,
-    Invoice,
-    Journal,
-    JournalLine,
-    LedgerAccount,
-    OfficeTask,
-    PayrollRecord,
-    PurchaseOrder,
-    SalesOrder,
-    WorkflowApproval,
+    AccountingEntry, AuditLog, CashTransaction, Company, CompanyDelegation,
+    Employee, InventoryItem, Invoice, Journal, JournalLine, LedgerAccount,
+    OfficeTask, PayrollRecord, PurchaseOrder, SalesOrder, WorkflowApproval,
 )
 
-admin.site.register(Company)
-admin.site.register(CompanyDelegation)
-admin.site.register(LedgerAccount)
-admin.site.register(AccountingEntry)
-admin.site.register(Journal)
-admin.site.register(JournalLine)
-admin.site.register(OfficeTask)
-admin.site.register(Invoice)
-admin.site.register(Employee)
-admin.site.register(InventoryItem)
-admin.site.register(CashTransaction)
-admin.site.register(AuditLog)
-admin.site.register(WorkflowApproval)
-admin.site.register(PayrollRecord)
-admin.site.register(PurchaseOrder)
-admin.site.register(SalesOrder)
+for model in (
+    Company, CompanyDelegation, LedgerAccount, AccountingEntry, Journal, JournalLine,
+    OfficeTask, Invoice, Employee, InventoryItem, CashTransaction, AuditLog,
+    WorkflowApproval, PayrollRecord, PurchaseOrder, SalesOrder,
+):
+    admin.site.register(model)
