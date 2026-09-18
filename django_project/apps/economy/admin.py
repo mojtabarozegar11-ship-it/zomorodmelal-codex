bda79ef9dd805a6951953f005fd63dbf0899f340
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-18 04:35:58 +0330

    Complete economic innovation and compliance workflow

--- django_project/apps/economy/admin.py
+++ django_project/apps/economy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Asset, EconomicAuditLog, EconomicReport, MarketDataSnapshot, OrderIntent, Portfolio, Position, RiskPolicy, Signal, SignalProduct, SignalPurchase, SignalSubscription, SignalCoupon, SignalPerformance, Strategy, Trade
+from .models import Asset, EconomicAuditLog, EconomicPublication, EconomicReport, EconomicWorkItem, MarketDataSnapshot, OrderIntent, Portfolio, Position, RiskPolicy, Signal, SignalCoupon, SignalPerformance, SignalProduct, SignalPurchase, SignalSubscription, Strategy, Trade, VirtualAssetProject
 
-for model in (Asset, MarketDataSnapshot, Strategy, RiskPolicy, Portfolio, Position, OrderIntent, Trade, EconomicReport, EconomicAuditLog, SignalProduct, SignalPurchase, SignalSubscription, SignalCoupon, SignalPerformance, Signal):
+for model in (Asset, MarketDataSnapshot, Strategy, RiskPolicy, Portfolio, Position, OrderIntent, Trade, EconomicReport, EconomicAuditLog, SignalProduct, SignalPurchase, SignalSubscription, SignalCoupon, SignalPerformance, Signal, VirtualAssetProject, EconomicWorkItem, EconomicPublication):
     admin.site.register(model)
