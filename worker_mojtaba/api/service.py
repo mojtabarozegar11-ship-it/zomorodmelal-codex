bd7a187bb3bdef9d0efae7b7ff991d78f1d7257a
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-19 21:31:46 +0330

    Register social publishing in Worker service

--- worker_mojtaba/api/service.py
+++ worker_mojtaba/api/service.py
@@ -11,6 +11,7 @@ from worker_mojtaba.tools.android_adapter import AndroidToolAdapter
 from worker_mojtaba.tools.automation_adapter import AutomationToolAdapter
 from worker_mojtaba.tools.research_adapter import ResearchToolAdapter
 from worker_mojtaba.tools.document_adapter import DocumentToolAdapter
+from worker_mojtaba.tools.social_adapter import SocialPublishingToolAdapter
 from worker_mojtaba.wallet.wallet import WalletManager
 from worker_mojtaba.tools.iranian_bank_adapter import IranianBankAdapter, BankAccountPolicy
 from worker_mojtaba.tools.financial_adapter import FinancialToolAdapter
@@ -44,6 +45,7 @@ class WorkerService:
         self.tool_center.register(AutomationToolAdapter())
         self.tool_center.register(ResearchToolAdapter())
         self.tool_center.register(DocumentToolAdapter())
+        self.tool_center.register(SocialPublishingToolAdapter())
         self.tools.register(
             "echo",
             "Safe diagnostic tool that returns the received request.",
