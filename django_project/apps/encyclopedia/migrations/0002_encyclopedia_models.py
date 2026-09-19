d09c2d5d6905bee7a052dbf6446f8e70688f9686
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-19 05:28:08 +0330

    Normalize encyclopedia migrations

--- django_project/apps/encyclopedia/migrations/0002_encyclopedia_models.py
+++ django_project/apps/encyclopedia/migrations/0002_encyclopedia_models.py
@@ -1,27 +1,6 @@
-from django.db import migrations, models
-import django.db.models.deletion
+from django.db import migrations
+
 
 class Migration(migrations.Migration):
     dependencies = [("encyclopedia", "0001_initial")]
-    operations = [
-        migrations.DeleteModel(name="Article"),
-        migrations.CreateModel(name="EncyclopediaCategory", fields=[
-            ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
-            ("title", models.CharField(max_length=200)), ("slug", models.SlugField(unique=True)),
-            ("description", models.TextField(blank=True)),
-        ]),
-        migrations.CreateModel(name="EncyclopediaArticle", fields=[
-            ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
-            ("title", models.CharField(max_length=300)), ("slug", models.SlugField(unique=True)),
-            ("summary", models.TextField(blank=True)), ("content", models.TextField()),
-            ("keywords", models.TextField(blank=True)), ("sources", models.TextField(blank=True)),
-            ("published", models.BooleanField(default=False)), ("created_at", models.DateTimeField(auto_now_add=True)),
-            ("updated_at", models.DateTimeField(auto_now=True)),
-            ("category", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="encyclopedia.encyclopediacategory")),
-        ]),
-        migrations.CreateModel(name="KnowledgeRelation", fields=[
-            ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
-            ("article", models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to="encyclopedia.encyclopediaarticle")),
-            ("related_article", models.ForeignKey(related_name="relations", on_delete=django.db.models.deletion.CASCADE, to="encyclopedia.encyclopediaarticle")),
-        ]),
-    ]
+    operations = []
