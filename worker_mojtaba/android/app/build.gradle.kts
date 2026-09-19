fdd59c2860e949753a4835d41c0b759df9a28893
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-19 21:22:52 +0330

    Align Compose compiler with Kotlin 1.9.25

--- worker_mojtaba/android/app/build.gradle.kts
+++ worker_mojtaba/android/app/build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android { namespace = "com.mojtaba.worker"; compileSdk = 35
+    composeOptions { kotlinCompilerExtensionVersion = "1.5.15" }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget = "17" }
     defaultConfig {
