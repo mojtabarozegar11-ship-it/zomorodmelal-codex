6c62cc7de36bb3c98bc660c3ff2fa0cb4b68bc6c
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-19 05:12:31 +0330

    Fix Django Passenger WSGI entrypoint

--- django_project/passenger_wsgi.py
+++ django_project/passenger_wsgi.py
@@ -1,17 +1,13 @@
-
-"""Passenger WSGI entrypoint for the Zomorod Melal Django project."""
+"""Passenger entrypoint for the Zomorod Melal Django project."""
 
 import os
 import sys
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-for path in (BASE_DIR, os.path.dirname(BASE_DIR)):
-    if path not in sys.path:
-        sys.path.insert(0, path)
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-import config.wsgi
-
-application = config.wsgi.application
+from config.wsgi import application
