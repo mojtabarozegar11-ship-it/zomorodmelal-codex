7a1bdffc655d67aadef2ae66f528437c56fafcfe
mojtabarozegar11-ship-it <mojtaba.rozegar11@gmail.com>
2026-09-19 20:14:16 +0330

    Add Android coroutine runtime

--- worker_mojtaba/android/app/build.gradle.kts
+++ worker_mojtaba/android/app/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.7.6")
     implementation("androidx.compose.material3:material3:1.3.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
